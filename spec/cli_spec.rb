4caa77b98ebcb12e2a9acb979ebf585d25da00bb
kyoendo <postagie@gmail.com>
2014-06-18 21:42:31 +0900

    add example for save option of get

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,20 +4,34 @@ describe GhDiff::CLI do
   before do
     $stdout, $stderr = StringIO.new, StringIO.new
     @original_dir = Dir.pwd
+    @save_dir = File.join(source_root, "diff")
     Dir.chdir(source_root)
     Octokit.reset!
   end
 
   after do
     $stdout, $stderr = STDIN, STDERR
+    FileUtils.rm_r(@save_dir) if Dir.exist?(@save_dir)
     Dir.chdir(@original_dir)
   end
 
   describe "get" do
-    it "gets a file content" do
+    it "prints a file content" do
       VCR.use_cassette 'quickstart' do
-        GhDiff::CLI.start(['get', 'docs/quickstart.md', '--repo=jekyll/jekyll', '--path=site'])
+        GhDiff::CLI.start(['get', 'docs/quickstart.md',
+                           '--repo=jekyll/jekyll', '--path=site'])
         expect($stdout.string).to match(/title: Quick-start guide/)
+      end
+    end
+
+    it "saves a file content" do
+      VCR.use_cassette 'quickstart' do
+        GhDiff::CLI.start(['get', 'docs/quickstart.md',
+                           '--repo=jekyll/jekyll', '--path=site',
+                           '--save'])
+        path = 'diff/docs/quickstart.md'
+        expect(File.exist? path).to be true
+        expect(File.read path).to match(/title: Quick-start guide/)
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'gh-diff'
 require "stringio"
-# require "fakeweb"
+require "fileutils"
 require "webmock"
 require "vcr"
 
