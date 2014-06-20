c187a2a5849f82cc035d3c14bcc33a2d6328627c
kyoendo <postagie@gmail.com>
2014-06-20 20:59:22 +0900

    add examples for option class

---
+++ spec/option_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe GhDiff::Option do
+  before(:all) do
+    @dotenv = File.join(source_root, ".env")
+    File.write(@dotenv, <<-EOS)
+REPO=jekyll/jekyll
+DIR=site
+    EOS
+    @dotenv_opts = {'repo' => 'jekyll/jekyll', 'dir' => 'site'}
+    @global_opts = {'token' => '12345'}
+    ENV['GH_TOKEN'] = '12345'
+    ENV['GH_DIR'] = 'lib'
+  end
+
+  after(:all) do
+    FileUtils.rm(@dotenv) if File.exist?(@dotenv)
+  end
+
+  let(:option) do
+    GhDiff::Option.new({user:'Charlie'})
+  end
+
+  describe "#dotenv" do
+    it "returns dotenv variables" do
+      expect(option.dotenv).to eq(@dotenv_opts)
+    end
+  end
+
+  describe "#env" do
+    it "returns env variables" do
+      opts = @global_opts.merge(@dotenv_opts)
+      expect(option.env).to eq(opts)
+    end
+  end
+end
