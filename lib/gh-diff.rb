56c3f75dfbeb5e809072e0c7400e16f0b4561d3f
kyoendo <postagie@gmail.com>
2014-06-20 09:38:56 +0900

    implement #get

--- lib/gh-diff.rb
+++ lib/gh-diff.rb
@@ -1,6 +1,27 @@
 require "gh-diff/version"
 require "gh-diff/cli"
 
-module GhDiff
+require "base64"
 
+module GhDiff
+  class GhDiff
+    def initialize(repo, opts)
+      @repo = repo
+      @opts = opts
+    end
+
+    def get(file)
+      path, ref = build_path(@opts[:dir], file), @opts[:revision]
+      f = Octokit.contents(@repo, path:path, ref:ref)
+      Base64.decode64(f.content)
+    end
+
+    def build_path(dir, file)
+      if dir.nil? || dir.empty?
+        file
+      else
+        File.join(dir, file)
+      end
+    end
+  end
 end
