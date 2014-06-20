f7768922a28dd7507d88cbcd5ce06f7dd991e777
kyoendo <postagie@gmail.com>
2014-06-20 09:38:30 +0900

    add example for GhDiff#get

--- spec/gh-diff_spec.rb
+++ spec/gh-diff_spec.rb
@@ -5,3 +5,16 @@ describe GhDiff do
     expect(GhDiff::VERSION).not_to be nil
   end
 end
+
+describe GhDiff::GhDiff do
+  let(:gh) { GhDiff::GhDiff.new 'jekyll/jekyll', revision:'master', dir:'site' }
+
+  describe "get" do
+    it "returns file content" do
+      VCR.use_cassette('quickstart') do
+        content = gh.get('docs/quickstart.md')
+        expect(content).to match(/title: Quick-start guide/)
+      end
+    end
+  end
+end
