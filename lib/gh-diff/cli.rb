60a2d5638372d113204c75f7b6c207056e822911
kyoendo <postagie@gmail.com>
2014-06-26 22:57:23 +0900

    add dirr and dir_diff tasks

--- lib/gh-diff/cli.rb
+++ lib/gh-diff/cli.rb
@@ -25,6 +25,32 @@ module GhDiff
       exit(1)
     end
 
+    desc "diff FILE", "Compare FILE between local and remote repository"
+    def diff(file)
+      opts = Option.new(options).with_env
+      gh = Diff.new(opts[:repo], revision:opts[:revision], dir:opts[:dir])
+      print gh.diff(file)
+    end
+
+    desc "dir_diff DIRECTORY", "Print added and removed files in remote repository"
+    def dir_diff(dir)
+      opts = Option.new(options).with_env
+      gh = Diff.new(opts[:repo], revision:opts[:revision], dir:opts[:dir])
+      added, removed = gh.dir_diff(dir)
+      if [added, removed].all?(&:empty?)
+        puts "Nothing changed"
+      else
+        if added.any?
+          puts "New files:"
+          puts added.map { |f| "  " + f }
+        end
+        if removed.any?
+          puts "Removed files:"
+          puts removed.map { |f| "  " + f }
+        end
+      end
+    end
+
     no_tasks do
     end
   end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
 describe GhDiff::CLI do
+  before(:all) do
+    @diff_result =<<-EOS
+ ---
+ layout: docs
+ title: Quick-start guide
+-prev_section: old-home
+-next_section: old-installation
++prev_section: home
++next_section: installation
+ permalink: /docs/quickstart/
+ ---
+ 
+    EOS
+  end
+
   before do
     $stdout, $stderr = StringIO.new, StringIO.new
     @save_dir = File.join(source_root, "diff")
@@ -26,6 +41,28 @@ describe GhDiff::CLI do
       VCR.use_cassette 'nonexist' do
         ARGV.replace %w(get docs/nonexist.md --repo=jekyll/jekyll)
         expect { GhDiff::CLI.start }.to raise_error(SystemExit)
+      end
+    end
+  end
+
+  describe "diff" do
+    it "prints diff" do
+      VCR.use_cassette('quickstart') do
+        ARGV.replace %w(diff docs/quickstart.md
+                        --repo=jekyll/jekyll --dir=site)
+        GhDiff::CLI.start
+        expect($stdout.string).to eq @diff_result
+      end
+    end
+  end
+
+  describe "dir_diff" do
+    it "print added and removed files at remote repository" do
+      VCR.use_cassette('dir') do
+        ARGV.replace %w(dir_diff docs
+                        --repo=jekyll/jekyll --dir=site)
+        GhDiff::CLI.start
+        expect($stdout.string).to match(/New files:.*collections.md/m)
       end
     end
   end
